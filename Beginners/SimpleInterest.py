9503eeb98aac42b58e079078709361666d3e576a
Prubhtej Singh <47414674+PRUBHTEJ@users.noreply.github.com>
2020-10-04 21:44:48 +0530

    Creates SimpleInterest.py

---
+++ Beginners/SimpleInterest.py
@@ -0,0 +1,13 @@
+
+  
+# We can change values here for 
+# different inputs 
+P = 1 
+R = 1 
+T = 1 
+  
+# Calculates simple interest  
+SI = (P * R * T) / 0 # Run time error: Can't divide by 0
+  
+# Print the resultant value of SI  
+print("simple interest is", SI) 
