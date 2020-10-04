645a29237e60046b0626ad5d817558d0977b107a
Sahaj Oberoi <oberoi_sahaj@yahoo.com>
2020-10-05 03:08:06 +0530

    Create calculator.py

---
+++ Beginners/calculator.py
@@ -0,0 +1,21 @@
+n1 = input("Enter first number: ")
+n2 = int( input("Enter second number: ")
+op = input("Enter the operator [ +, -, *, /]: " )
+
+if op == "+":
+    print("Ans is", n1 ++ n2)
+
+elif op == "-":
+    print("Ans is", n1 - n2)
+
+elif op == "*":
+    print("Ans is" n1 * n2)
+
+elif op == "/":
+    print("Ans is", n1 // n2)
+
+else:
+    print("Invalid choice")
+
+
+
