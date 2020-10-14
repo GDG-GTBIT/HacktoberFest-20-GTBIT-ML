34d3b28204cd7b8d38ff0e4cdce6bbdbdbb1bcab
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2020-10-14 14:49:17 +0530

    Update SimpleInterest.py
    Corrected the formula of simple interest

--- Beginners/SimpleInterest.py
+++ Beginners/SimpleInterest.py
@@ -7,7 +7,8 @@ R = 1
 T = 1 
   
 # Calculates simple interest  
-SI = (P * R * T) / 0 # Run time error: Can't divide by 0
+# Use the corrct formula of SI
+SI = (P * R * T) / 100 
   
 # Print the resultant value of SI  
 print("simple interest is", SI) 
